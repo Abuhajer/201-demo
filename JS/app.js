28d2491eb81e48174fb99b7542129d477b7d9128
Dana Kiswani <danakiswani0@gmail.com>
2020-09-07 22:40:45 +0300

    Added Some Prompts

---
+++ JS/app.js
@@ -0,0 +1,78 @@
+'use strict';
+
+
+alert('Hey Youuuuu ,Welcome to My wibsite');
+var dana = prompt('What was you do in the quarantine ??'+'[study/read/take a course/playing games/eat/watch movies/watch serise]').toLowerCase();
+// console.log(dana);
+
+if(dana == 'read' || dana == 'study' || dana == 'take a course'){
+  alert('Excellent, you haven\'t wasted your time');
+} else if (dana == 'eat' || dana == 'playing games'){
+  alert('Hhhh thats cool .. sa7teen btw');
+} else if (dana == 'watch movies' || dana == 'watch serise'){
+  alert('WOW, I love that ,Tell me its name please ');
+} else {
+  alert('Sorry I cant\'t approve you');
+}
+
+
+// ----------------------------------------------
+
+
+ var user = confirm('Do you want to know alot about me? =)');
+//  console.log(user);
+
+ if(user === true){
+  alert('OH! Thanx Dear welcome welcome');
+   } else {
+  alert('Oops! WHYYYYYYYY =0');
+    }
+
+  //  ------------------------------------------------
+
+    var user = confirm('Mmmm , Do you love Software??');
+    // console.log(user);
+
+    if(user === true){
+     alert('OH! nice i\'m software-developer');
+      } else {
+     alert('Oops! WHYYYYYYYY =0');
+       }
+
+
+      //  ------------------------------------------------
+
+
+       var user = confirm('Do you know Arab-Bank & Zain-Telecom ??');
+      //  console.log(user);
+
+       if(user === true){
+        alert('dandy! I\'m trained thier ');
+         } else {
+        alert('Oops! GOOGLE it');
+          }
+
+
+          // ------------------------------------------------
+
+
+          var user = confirm('Do you active on social-media?');
+          // console.log(user);
+
+          if(user === true){
+           alert('OH! Nice');
+            } else {
+           alert('Oops!');
+             }
+
+               // ------------------------------------------------
+
+
+          var user = confirm('Are you bored of my questions? ');
+          // console.log(user);
+
+          if(user === true){
+           alert('I\'m Sorry for your time');
+            } else {
+           alert('Thank you');
+             }
