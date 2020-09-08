f3e0563399bc78bc1f124658e693551744a018f0
Dana Kiswani <danakiswani0@gmail.com>
2020-09-08 16:28:10 +0300

    Change the prompt

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,5 @@
 'use strict';
-
+var count=0;
 
 alert('Hey Youuuuu ,Welcome to My wibsite');
 var dana = prompt('What was you do in the quarantine ??'+'[study/read/take a course/playing games/eat/watch movies/watch serise]').toLowerCase();
@@ -7,72 +7,115 @@ var dana = prompt('What was you do in the quarantine ??'+'[study/read/take a cou
 
 if(dana == 'read' || dana == 'study' || dana == 'take a course'){
   alert('Excellent, you haven\'t wasted your time');
+  
 } else if (dana == 'eat' || dana == 'playing games'){
   alert('Hhhh thats cool .. sa7teen btw');
+  
 } else if (dana == 'watch movies' || dana == 'watch serise'){
   alert('WOW, I love that ,Tell me its name please ');
+  
 } else {
   alert('Sorry I cant\'t approve you');
 }
 
-
 // ----------------------------------------------
 
 
- var user = confirm('Do you want to know alot about me? =)');
-//  console.log(user);
+ var user = prompt('Do you want to know more about me ??').toLowerCase();
+  //  console.log(user);
 
- if(user === true){
+  if(user === "yes" || user === "y"){
   alert('OH! Thanx Dear welcome welcome');
-   } else {
+   count++;
+   } else if (  user === "no" || user === "n") {
   alert('Oops! WHYYYYYYYY =0');
+   }
+  else alert ("Wrong");
+    
+
+    //  ------------------------------------------------
+
+    var user = prompt('Mmmm , Do i love Software??').toLowerCase();
+    // console.log(user);
+    switch (true){
+    case (user === "yes" || user === "y"): 
+    alert('OH! nice i\'m software-developer');
+    count++;
+    break;
+
+    case (  user === "no" || user === "n"):
+    alert('Oops! WHYYYYYYYY =0');
+    break;
+    default : alert("wrong input");
     }
 
-  //  ------------------------------------------------
-
-    var user = confirm('Mmmm , Do you love Software??');
-    // console.log(user);
-
-    if(user === true){
-     alert('OH! nice i\'m software-developer');
-      } else {
-     alert('Oops! WHYYYYYYYY =0');
-       }
+    // ------------------------------------------------
 
 
-      //  ------------------------------------------------
-
-
-       var user = confirm('Do you know Arab-Bank & Zain-Telecom ??');
+      var user = prompt('Do you know Arab-Bank & Zain-Telecom ??').toLowerCase();
       //  console.log(user);
 
-       if(user === true){
-        alert('dandy! I\'m trained thier ');
-         } else {
-        alert('Oops! GOOGLE it');
-          }
+      if(user === "yes" || user === "y"){
+      alert('dandy! I\'m trained thier ');
+      count++;
+
+      } else if(user === "no" || user === "n") {
+      alert('Oops! GOOGLE it');
+      }
+      else alert("??");
 
 
-          // ------------------------------------------------
+     // ------------------------------------------------
 
 
-          var user = confirm('Do you active on social-media?');
+
+          var user = prompt('Do you active on social-media?').toLowerCase();
           // console.log(user);
 
-          if(user === true){
+          if(user === "yes" || user === "y"){
            alert('OH! Nice');
-            } else {
+           count++;
+
+            } else if ( user === "no" || user === "n") {
            alert('Oops!');
              }
+             else alert ("??");
 
-               // ------------------------------------------------
 
+     // ------------------------------------------------
+    
 
-          var user = confirm('Are you bored of my questions? ');
+          var user = prompt('Are you bored of my questions? ').toLowerCase();
           // console.log(user);
 
-          if(user === true){
+          if(user === "yes" || user === "y"){
            alert('I\'m Sorry for your time');
-            } else {
+           count++;
+            } else if(user === "no" || user === "n") {
            alert('Thank you');
              }
+             else alert("??");
+
+    //  -----------------------------------
+
+
+//  var guess= [1,2,3,4,5,6,7,8,9,10];
+//  var usernum=prompt("Can you guess a number from 1 to 10 thats in my mind..!");
+//      usernum=Number(usernum);
+
+// for(var h=0; h < 4 ;h++){
+// console.log(h);
+// }
+
+
+
+
+
+
+
+
+
+
+            
+alert ("your score is"+ count + "/7");
+
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
+Project Name: 📂
+About Me, with a guessing game.
 
+Author: 📂
+Dana Kiswani
+
+<p>Teach us more about you! Build out an “About Me” webpage that quizzes the user on fun facts gives the user some basic information about who you are and where you came from. We will build onto this project over the next few labs. Be prepared to refactor your code each day and learn about how a web site incrementally goes from an idea to a deployed site through various iterations of development.</p>
 
